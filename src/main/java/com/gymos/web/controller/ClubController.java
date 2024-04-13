88639e04909f36023a0711b487b35ae016b09bb1
FirmestTexas79 <matasovsky.samuel@seznam.cz>
2024-04-13 11:55:08 +0200

    Form post

--- src/main/java/com/gymos/web/controller/ClubController.java
+++ src/main/java/com/gymos/web/controller/ClubController.java
@@ -6,6 +6,8 @@ import com.gymos.web.service.ClubService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -29,5 +31,11 @@ public class ClubController {
         Club club = new Club();
         model.addAttribute("club", club);
         return "clubs-create";
+    }
+
+    @PostMapping("/clubs/new")
+    public String saveClub(@ModelAttribute("club")Club club){
+        clubService.saveClub(club);
+        return "redirect:/clubs";
     }
 }
--- src/main/java/com/gymos/web/service/ClubService.java
+++ src/main/java/com/gymos/web/service/ClubService.java
@@ -1,9 +1,11 @@
 package com.gymos.web.service;
 
 import com.gymos.web.dto.ClubDto;
+import com.gymos.web.models.Club;
 
 import java.util.List;
 
 public interface ClubService {
     List<ClubDto> findAllClubs();
+    Club saveClub(Club club);
 }
